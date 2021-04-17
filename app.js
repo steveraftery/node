619b9958597a9e3dd8ed5b837619aa41b0d2ef14
steveraftery <steveraftery@yahoo.com>
2021-04-17 18:55:51 -0500

    updating port

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.get('/', (req,res) => {
     res.send("Welcome to the homepage Dude");
 });
 
-const port = process.env.port || 3000;
+const port = process.env.port || 8080;
 
 app.listen(port,() => {
     console.log("Wazzapppppp dude");
